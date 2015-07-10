f6753b50d505141d75bfff45deaab87a26e195de
Lior Kesos <lior@linnovate.net>
2015-07-10 05:44:48 +0000

    syncing with git + changed link of releases

--- packages/system/public/controllers/goto.js
+++ packages/system/public/controllers/goto.js
@@ -1,5 +1,6 @@
 angular.module('mean.system').controller('GoToController', ['$scope', 'Global', '$location', '$anchorScroll', function ($scope, Global, $location, $anchorScroll) {
     $scope.global = Global;
+	$anchorScroll.yOffset = 150;
     $scope.goto = function(elementId) {
         // set the location.hash to the id of
         // the element you wish to scroll to.
